96ba26c7212075cf8c39b246ee3aec745adc132d
Etudiant <Etudiant>
2017-04-10 17:21:44 +0200

    eval blanche

--- eval blanche/js/script.js
+++ eval blanche/js/script.js
@@ -1,43 +1,100 @@
 $(document).ready( function(){
 
+// fermer la modal
+$('.fa-times').click(function(){
+    $('div').fadeOut();
+});
+// supprimer les class error
+ $('input,select, textarea').focus(function(){
+     $(this).removeClass('error');
+ });
+    // capter la soumission du formulaire
  $('form').submit(function(event){
+    //  bloquer le comportement naturel du formulaire
     event.preventDefault();
 
+    // définir les variables globales
+    var userName= $('[type="text"]');
+    var userEmail= $('[type="email"]');
+    var userSubject= $('select');
+    var userMessage= $('textarea');
+    var formScore = 0;
+
+    // vérifier que l'utilisateur a bien saisi son nom
+
     if ($('[type="text"]').val().length < 4){
-        console.log('your name : au moins 6 caractéres');
+        // ajouter la class error dans l'input
+        userName.addClass('error');
+        
     }else {
-        console.log('nom validé !');
+        
+        // incrémenter la valeur de la variable formScore
+        formScore++;
     };
 
 
 
     if ($('[type="email"]').val().length < 9){
-        console.log('email: au moins 9 caractéres');
+         // ajouter la class error dans l'input
+        userName.addClass('error');
+        
     }else {
-        console.log('email validé!');
+       
+        // incrémenter la valeur de la variable formScore
+        formScore++;
     };
 
 
 
-    if ($('select').val()==""){
-        console.log('il faut choisir un pays');
+    if ($('select').val()=="null"){
+        // ajouter la class error dans l'input
+        userName.addClass('error');
+        
     }else{
-        console.log('country choisi !');
+        
+        // incrémenter la valeur de la variable formScore
+        formScore++;
+
     };
-
-
+        
 
 
     if ($('textarea').val().length < 15){
-        console.log('message:au moins 15 caractéres');
+         // ajouter la class error dans l'input
+        userName.addClass('error');
+        
     }else if ($('textarea').val().length > 70){
-        console.log('message:maximum 70 caractéres');
+        
     }else{
-        console.log('message validé !');
+       
+        // incrémenter la valeur de la variable formScore
+        formScore++;
     };
     
+    // validation finale du formulaire
+    if(formScore == 4){
+        console.log('le formulaire est validé !');
+    };
 
-     if(
+    // envoi des données dans le fichier de traitement PHP
+    // PHP répond true==> continuer le traitement du formulaire
+
+
+    // afficher les données du formulaire dans une modal
+
+    $('div span').text(userName.val() );
+    $('div b').text(userSubject.val() );
+    $('div p:last').text(userMessage.val() );
+
+    // afficher la modal
+    $('div').fadeIn();
+
+    // vider les champs du formulaire
+    $('form')[0].reset();
+
+    userEmail.val('');
+
+
 
 });
 
